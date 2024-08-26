ae3c85f881082432a61f173da227f0ac111abb21
Dan-RAI <20709058+Dan-RAI@users.noreply.github.com>
2024-08-26 17:00:37 +0200

    Missing pgxc_ctl added

---
+++ contrib/pgxc_ctl/signature.h
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------
+ *
+ * signature.h
+ *
+ *    Signature of module of Postgres-XC configuration and operation tool.
+ *
+ * Copyright (c) 2013 Postgres-XC Development Group
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef SIGNATURE_H
+#define SIGNATURE_H
+/* Signature file to identify the make */
+#define signature "150623_1800_391046716"
+#endif /* SIGNATURE_H */
